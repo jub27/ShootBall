43cb559f8663795db8a597eaeec864a0e06e0ab6
DESKTOP-9T5OK38\dosss <dosss2726@gmail.com>
2023-11-30 22:55:34 +0900

    메인화면 구현중, 로컬라이즈 패키지 적용중

--- DrawPath/Assets/Scripts/MainMenuSceneManager.cs
+++ DrawPath/Assets/Scripts/MainMenuSceneManager.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuSceneManager : MonoBehaviour
 {
+    private const float BACKGROUND_SCROLL_SPEED = 0.3f;
+    [SerializeField]
+    private Image _backgroundImage;
+    private float passedTime;
+
+    private void Awake()
+    {
+        passedTime = 0;
+    }
+
+    private void Update()
+    {
+        passedTime += Time.deltaTime;
+        _backgroundImage.material.SetTextureOffset("_MainTex", new Vector2(BACKGROUND_SCROLL_SPEED * passedTime, 0));
+    }
+
     public void OnGamePlayButtonClick()
     {
         SceneManager.LoadScene("GamePlayScene");
